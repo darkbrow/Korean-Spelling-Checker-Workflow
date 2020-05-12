7100df8e8934ad3f2f25dd3892b253a8b670f796
darkbrow <darkbrow@gmail.com>
2020-05-13 05:43:38 +0900

    workaround for curl error

--- ✔ 선택한 글의 한국어 맞춤법 검사하기.workflow/Contents/checker.sh
+++ ✔ 선택한 글의 한국어 맞춤법 검사하기.workflow/Contents/checker.sh
@@ -5,6 +5,7 @@ CHECK_URL=$BASE_URL/results
 KSC_PATH="$(pwd)/Library/Services/✔\ 선택한\ 글의\ 한국어\ 맞춤법\ 검사하기.workflow/Contents"
 SCRIPT_PATH=$KSC_PATH/script.js
 STYLE_PATH=$KSC_PATH/style.css
-UPDATE=2019-03-28
-UPDATE_STATUS=$(curl -s -o /dev/null -I -w "%{http_code}" -m 3 https://appletree.or.kr/automator/$UPDATE.txt)
-curl --data-urlencode "text1=$INPUT_TEXT" $CHECK_URL -o .cache/korean-spelling-checker-result.html && sed -i -e "s|\.\./|$BASE_URL/|g" .cache/korean-spelling-checker-result.html && sed -E -i -e "s (css|js)/ $BASE_URL/\1/ g" .cache/korean-spelling-checker-result.html && sed -i -e "s|<body|<link rel='stylesheet' href='$STYLE_PATH'><script defer src='$SCRIPT_PATH' id='ksc' data-update-status='$UPDATE_STATUS'></script><base href='$BASE_URL'></head><body|" .cache/korean-spelling-checker-result.html && echo "$(<~/.cache/korean-spelling-checker-result.html)"
+UPDATE=2020-05-13
+UPDATE_STATUS="Current"
+#UPDATE_STATUS=$(curl -s -o /dev/null -I -w "%{http_code}" -m 3 https://appletree.or.kr/automator/$UPDATE.txt)
+curl --data-urlencode "text1=$INPUT_TEXT" $CHECK_URL -o ~/.cache/korean-spelling-checker-result.html && sed -i -e "s|\.\./|$BASE_URL/|g" ~/.cache/korean-spelling-checker-result.html && sed -E -i -e "s (css|js)/ $BASE_URL/\1/ g" ~/.cache/korean-spelling-checker-result.html && sed -i -e "s|<body|<link rel='stylesheet' href='$STYLE_PATH'><script defer src='$SCRIPT_PATH' id='ksc' data-update-status='$UPDATE_STATUS'></script><base href='$BASE_URL'></head><body|" ~/.cache/korean-spelling-checker-result.html && echo "$(<~/.cache/korean-spelling-checker-result.html)"
